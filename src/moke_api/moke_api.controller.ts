53a1a8103414fb07f3978c8fd28065a0861cebac
EbsaT <ebsa16teklu@gmail.com>
2023-05-25 14:48:03 +0300

    making payment controller and service

--- src/moke_api/moke_api.controller.ts
+++ src/moke_api/moke_api.controller.ts
@@ -1,16 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete,Headers, Res } from '@nestjs/common';
+import { Controller, Post, Body, Headers } from '@nestjs/common';
 import { MokeApiService } from './moke_api.service';
-import { CreateMokeApiDto } from './dto/create-moke_api.dto';
-import { UpdateMokeApiDto } from './dto/update-moke_api.dto';
-import { ApplyFabricTokenDto, ApplyFabricTokenHeaderDto } from './dto/applyFabricToken.dto';
-import { Response } from 'express';
+import { ApplyFabricTokenDto } from './dto/applyFabricToken.dto';
 
 @Controller('moke-api')
 export class MokeApiController {
   constructor(private readonly mokeApiService: MokeApiService) {}
 
   @Post('applyFabricToken')
-  applyFabricToken(@Body() body: ApplyFabricTokenDto, @Headers('X-APP-Key') headers: any
+  applyFabricToken(
+    @Body() body: ApplyFabricTokenDto,
+    @Headers('X-APP-Key') headers: any,
   ) {
     console.log(body.appSecret); // Output the request body to the console
     console.log(headers); // Output the request headers to the console
@@ -20,72 +19,124 @@ export class MokeApiController {
         error_msg: 'string',
       };
     }
-    return this.mokeApiService.applyFabricToken(body.appSecret,headers)
+    return this.mokeApiService.applyFabricToken(body.appSecret, headers);
   }
-  @Post('preOrder')  
-  createOrder(@Body() body:any , @Headers('X-APP-Key') fabric_app_id: any,@Headers('Authorization') token: any)
-{
-  const biz = body.biz_content
-  if (!token || !fabric_app_id || !body || !biz || !body.timestamp || !body.method || body.method!="payment.preorder" || !body.nonce_str) {
-    return {
-        "error_code":"string",
-        "error_msg":"string"
-      }
-  } 
-  if(!body.sign_type || body.sign_type!="SHA256WithRSA"){
-    return {
-        "error_code":"string",
-        "error_msg":"sign type must be SHA256WithRSA"
-      }
+  //CreateOrder
+  @Post('preOrder')
+  createOrder(
+    @Body() body: any,
+    @Headers('X-APP-Key') fabric_app_id: any,
+    @Headers('Authorization') token: any,
+  ) {
+    const biz = body.biz_content;
+    if (
+      !token ||
+      !fabric_app_id ||
+      !body ||
+      !biz ||
+      !body.timestamp ||
+      !body.method ||
+      body.method != 'payment.preorder' ||
+      !body.nonce_str
+    ) {
+      return {
+        error_code: 'string',
+        error_msg: 'string',
+      };
+    }
+    if (!body.sign_type || body.sign_type != 'SHA256WithRSA') {
+      return {
+        error_code: 'string',
+        error_msg: 'sign type must be SHA256WithRSA',
+      };
+    }
+    if (!biz.trans_currency || biz.trans_currency != 'ETB') {
+      return {
+        error_code: 'string',
+        error_msg: 'transCurrency type must be ETB',
+      };
+    }
+    if (
+      !biz.total_amount ||
+      !biz.notify_url ||
+      !biz.title ||
+      !biz.business_type ||
+      !biz.trade_type
+    ) {
+      return {
+        error_code: 'string',
+        error_msg: 'Required parameter is missing or is incorrectly filled',
+      };
+    }
+    return this.mokeApiService.createOrder(
+      token,
+      fabric_app_id,
+      biz.appid,
+      biz.merch_code,
+      biz.merch_order_id,
+      {
+        total_amount: biz.total_amount,
+        title: biz.title,
+        notify_url: biz.notify_url,
+        business_type: biz.business_type,
+        trade_type: biz.trade_type,
+      },
+    );
   }
-  if(!biz.trans_currency || biz.trans_currency!="ETB"){
-    return {
-        "error_code":"string",
-        "error_msg":"transCurrency type must be ETB"
-      }
-  }
-  return this.mokeApiService.createOrder(token,fabric_app_id, biz.appid, biz.merch_code,biz.merch_order_id)
-}
-  @Post('queryOrder')  
-  queryOrder(@Body() body:any , @Headers('X-APP-Key') fabric_app_id: any,@Headers('Authorization') token: any)
-{
-  const biz = body.biz_content
-  if (!token || !fabric_app_id || !body || !biz || !body.timestamp || !body.method || body.method!="payment.queryorder" || !body.nonce_str || !body.version) {
-    return {
-        "error_code":"string",
-        "error_msg":"string"
-      }
-  } 
-  if(!body.sign_type || body.sign_type!="SHA256WithRSA"){
-    return {
-        "error_code":"string",
-        "error_msg":"sign type must be SHA256WithRSA"
-      }
-  }
-  return this.mokeApiService.queryOrder(token,fabric_app_id, biz.appid, biz.merch_code,biz.merch_order_id)
-}
-  @Post()
-  create(@Body() createMokeApiDto: CreateMokeApiDto) {
-    return this.mokeApiService.create(createMokeApiDto);
-  }
+  //payment
+  @Post('payment')
+  payment(
+    @Body() body: any,
+    @Headers('X-APP-Key') fabric_app_id: any,
+    @Headers('Authorization') token: any,
+  ) {
+    const rawRequest = body.rawRequest;
+    if (!token || !fabric_app_id || !rawRequest) {
+      return {
+        error_code: 'string',
+        error_msg: 'string',
+      };
+    }
+    rawRequest.con;
 
-  @Get()
-  findAll() {
-    return this.mokeApiService.findAll();
+    this.mokeApiService.payment(rawRequest);
   }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.mokeApiService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMokeApiDto: UpdateMokeApiDto) {
-    return this.mokeApiService.update(+id, updateMokeApiDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.mokeApiService.remove(+id);
+  //Query Order
+  @Post('queryOrder')
+  queryOrder(
+    @Body() body: any,
+    @Headers('X-APP-Key') fabric_app_id: any,
+    @Headers('Authorization') token: any,
+  ) {
+    const biz = body.biz_content;
+    if (
+      !token ||
+      !fabric_app_id ||
+      !body ||
+      !biz ||
+      !body.timestamp ||
+      !body.method ||
+      body.method != 'payment.queryorder' ||
+      !body.nonce_str ||
+      !body.version
+    ) {
+      return {
+        error_code: 'string',
+        error_msg: 'string',
+      };
+    }
+    if (!body.sign_type || body.sign_type != 'SHA256WithRSA') {
+      return {
+        error_code: 'string',
+        error_msg: 'sign type must be SHA256WithRSA',
+      };
+    }
+    return this.mokeApiService.queryOrder(
+      token,
+      fabric_app_id,
+      biz.appid,
+      biz.merch_code,
+      biz.merch_order_id,
+    );
   }
 }
