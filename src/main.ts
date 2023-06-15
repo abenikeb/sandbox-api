c3698e15f4b8fe4da37ab03f405892cb477894e7
abenikeb <abenikeb79@gmail.com>
2023-06-15 16:54:43 +0300

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,13 @@ var cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  app.use(
-    cors({
+  app.enableCors(
+    // cors(),
+    {
       origin: '*',
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       allowedHeaders: ['Content-Type', 'Authorization'],
-    }),
+    },
   ); // Use this after the variable declaration
 
   app.setGlobalPrefix('api');
