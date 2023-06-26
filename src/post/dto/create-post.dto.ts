43650bc127e9e8247bf2007203e4a103088d045c
EbsaT <ebsa16teklu@gmail.com>
2023-06-26 22:48:10 +0300

    post dto

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePostDto {}
+import { IsDate, IsNotEmpty, IsNumber } from 'class-validator';
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+  @IsNotEmpty()
+  title: string;
+  @IsNotEmpty()
+  content: string;
+  @IsNotEmpty()
+  author_id: string;
+  @IsDate()
+  @IsNotEmpty()
+  created_at: Date;
+  @IsDate()
+  updated_at: Date;
+}
